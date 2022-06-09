dde5d0015a93b21a42d7aabc832468a7cd21bb43
stefanocravotta <stefano.cravotta8@gmail.com>
2022-06-09 17:35:48 +0200

    bonus select

--- assets/api/api.php
+++ assets/api/api.php
@@ -3,9 +3,18 @@
 require_once __DIR__ . "/../db/albums_db.php";
 
 $result= [];
-$success= true;
-$error_msg = '';
+$genre = $_GET['genre'];
+
+if(empty($genre)){
+    $result = $albums_db;
+  }else{
+    foreach($albums_db as $album ){
+      if($album['genre'] == $genre){
+        $result[] = $album;
+      }
+    }
+};
 
 header('Content-Type: application/json');
 
-echo json_encode($albums_db);
+echo json_encode($result);
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,18 @@
 const app = new Vue({
     el: "#app",
     data : {
-        apiUrl : 'http://localhost:8888/Esercitazioni_Boolean/php-ajax-dischi/assets/api/api.php',
+        apiUrl : 'http://localhost:8888/Esercitazioni_Boolean/php-ajax-dischi/assets/api/api.php?genre=',
         albums : [],
-        error_msg : "",
-        success: true,
+        genre: ""
     },
     mounted(){
         this.getAPI();
     },
     methods: {
         getAPI(){
-            axios.get(this.apiUrl)
+            axios.get(this.apiUrl + this.genre)
             .then(r =>{
                 this.albums = r.data;
-                console.log(this.albums);
             })
         }
     }
