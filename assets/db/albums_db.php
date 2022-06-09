df082d7b5faca2e3eff48ff10af5e055333ec26e
stefanocravotta <stefano.cravotta8@gmail.com>
2022-06-09 16:16:22 +0200

    1 Milestone

--- assets/db/albums_db.php
+++ assets/db/albums_db.php
@@ -1,6 +1,5 @@
 <?php 
 
-
 /* **Prima Milestone:**
 Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
 In questa milestone non ci sarà nessuna chiamata axios il file php stamperà direttamente il “database”
